e7a2931a06710186a2bd5f2aced080ed381d87e6
Anton Kokhanivskyi <72139304+TrueBalkar@users.noreply.github.com>
2023-11-21 17:31:25 +0200

    Add files via upload

--- inference.py
+++ inference.py
@@ -1,20 +1,43 @@
-from keras.models import load_model
-import segmentation_models as sm
-from keras import backend as K
-import cv2
-from PIL import Image
-from patchify import patchify
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from pathlib import Path
-import matplotlib.pyplot as plt
-import pandas as pd
 import argparse
+from keras.models import load_model
+import numpy as np
+from PIL import Image
+from pathlib import Path
+from tensorflow.keras import backend as K
+import tensorflow as tf
+
+
+def jacard_coef(y_true, y_pred):
+    """
+    Jacard coefficient for model metrics.
+    """
+    y_true = K.cast(y_true, dtype=tf.float32)
+    y_pred = K.cast(y_pred, dtype=tf.float32)
+
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+
+    intersection = K.sum(y_true_f * y_pred_f)
+    union = K.sum(y_true_f) + K.sum(y_pred_f) - intersection
+
+    return (intersection + 1.0) / (union + 1.0)
+
+
+def dice_coef(y_true, y_pred, smooth):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    dice = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return dice
+
+
+def dice_coef_loss(y_true, y_pred, smooth=10e-6):
+    return 1 - dice_coef(y_true, y_pred, smooth)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("ImagePath", help="Path to the picture (.jpg).")
 parser.add_argument("ModelPath", help="Path to the model (.h5).")
-parser.add_argument("TrainShipSegmentationCSVPath", help="Path to train_ship_segmentations_v2.csv file.")
 args = parser.parse_args()
 
 path = Path(args.ImagePath)
@@ -35,115 +58,13 @@ if not path.is_file():
     print(f'The file {args.ModelPath} does not exist')
     exit()
 
-path = Path(args.TrainShipSegmentationCSVPath)
-
-if args.TrainShipSegmentationCSVPath[-4:] != ".csv":
-    print("Please enter .csv file")
-    exit()
-if not path.is_file():
-    print(f'The file {args.TrainShipSegmentationCSVPath} does not exist')
-    exit()
-
-
-def rle_decode(mask_rle, IMG_SIZE=(768, 768)):
-    """
-    mask_rle: run-length as string formated (start length)
-    shape: (height,width) of array to return
-    Returns numpy array, 255 - mask, 0 - background
-    """
-    s = mask_rle.split()
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
-    starts -= 1
-    ends = starts + lengths
-    img = np.zeros(IMG_SIZE[0]*IMG_SIZE[1])
-    for lo, hi in zip(starts, ends):
-        img[lo:hi] = 255
-    return img.reshape(IMG_SIZE).T
-
-
-# Load data from csv file, extract encoded pixels to create masks for training
-masks = pd.read_csv(args.TrainShipSegmentationCSVPath)
-masks['ships'] = masks['EncodedPixels'].map(lambda c_row: 1 if isinstance(c_row, str) else 0)
-unique_img_ids = masks.groupby('ImageId').agg({'ships': 'sum'}).reset_index()
-unique_img_ids['is_ship'] = unique_img_ids['ships'].map(lambda x: 1.0 if x > 0 else 0.0)
-
-
-def jacard_coef(y_true, y_pred):
-    """
-    Jacard coefficient for model metrics.
-    """
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
-
-
-# Parameters for model
-weights = [0.5, 0.5]
-dice_loss = sm.losses.DiceLoss(class_weights=weights)
-focal_loss = sm.losses.CategoricalFocalLoss()
-total_loss = dice_loss + (1 * focal_loss)
-
-# Load pretrained model
-model = load_model(args.ModelPath,
-                   custom_objects={'dice_loss_plus_1focal_loss': total_loss,
-                                   'jacard_coef': jacard_coef})
-
-testing_image_patches = []
-
-image = cv2.imread(args.ImagePath)  # Read each image as BGR
-SIZE_X = 768
-SIZE_Y = 768
-image = Image.fromarray(image)
-image = image.crop((0, 0, SIZE_X, SIZE_Y))  # Crop from top left corner
-patch_size = 256
-scaler = MinMaxScaler()
-image = np.array(image)
-
-# Extract patches from each image
-patches_img = patchify(image, (patch_size, patch_size, 3), step=patch_size)  # Step=256 for 256 patches means no overlap
-
-for i in range(patches_img.shape[0]):
-    for j in range(patches_img.shape[1]):
-        single_patch_img = patches_img[i, j, :, :]
-        # Use minmaxscaler instead of just dividing by 255.
-        single_patch_img = scaler.fit_transform(single_patch_img.reshape(-1, single_patch_img.shape[-1])).reshape(single_patch_img.shape)
-
-        # single_patch_img = (single_patch_img.astype('float32')) / 255.
-        single_patch_img = single_patch_img[0]  # Drop the extra unecessary dimension that patchify adds.
-        testing_image_patches.append(single_patch_img)
-
-testing_image_patches = np.array(testing_image_patches)
-
-buffer = np.zeros((9, 256, 256))
-for l in range(9):
-    prediction = (model.predict(np.expand_dims(testing_image_patches[l], 0)))
-    predicted_img = np.array(prediction)[0, :, :]
-    for i in range(256):
-        for j in range(256):
-            if predicted_img[i][j][0] < predicted_img[i][j][1]:
-                buffer[l][i][j] = 1
-
-pred_mask = np.block([[buffer[0].T, buffer[1].T, buffer[2].T],
-                      [buffer[3].T, buffer[4].T, buffer[5].T],
-                      [buffer[6].T, buffer[7].T, buffer[8].T]])
-
-img = cv2.imread(args.ImagePath)
-img_masks = masks.loc[masks['ImageId'] == args.ImagePath[-13:], 'EncodedPixels'].tolist()
-
-all_masks = np.zeros((768, 768))
-for mask in img_masks:
-    all_masks += rle_decode(mask, (768, 768))
-
-
-plt.figure(figsize=(15, 9))
-plt.subplot(131)
-plt.title("Actual Image:")
-plt.imshow(image)
-plt.subplot(132)
-plt.title("Actual Mask:")
-plt.imshow(all_masks)
-plt.subplot(133)
-plt.title("Predicted Mask:")
-plt.imshow(pred_mask)
-plt.show()
+model = load_model(args.ModelPath, custom_objects={
+    'dice_coef_loss': dice_coef_loss,
+    'jacard_coef': jacard_coef
+})
+image = np.array(Image.open(args.ImagePath))
+predicted_mask = model.predict(np.expand_dims(image, 0))
+predicted_mask *= 255
+predicted_mask = predicted_mask.reshape((768, 768)).astype('uint8')
+Image.fromarray(predicted_mask).show(title='Predicted Mask')
+Image.fromarray(image).show(title='Image')
--- train.py
+++ train.py
@@ -1,19 +1,100 @@
-import numpy as np
 import pandas as pd
-import os
+import numpy as np
 import cv2
+import os
 import shutil
-from keras.layers import Conv2D, Input, Dropout, concatenate, MaxPooling2D, Conv2DTranspose
-from keras import Model
-from tensorflow.keras.utils import to_categorical
-from patchify import patchify
-from PIL import Image
-import segmentation_models as sm
-from sklearn.preprocessing import MinMaxScaler
-from keras import backend as K
-from sklearn.model_selection import train_test_split
-from keras.metrics import MeanIoU
+import tensorflow as tf
+from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.metrics import Precision, Recall
+from tensorflow.keras import backend as K
 import argparse
+
+
+# Basic UNet model has over 31M+ params, so we need to simplify it
+# in order to train it faster and avoid overfitting
+def unet_model_simplified(input_shape=(768, 768, 3)):
+    # Define the input layer
+    inputs = tf.keras.Input(shape=input_shape)
+
+    # Contracting Path
+    conv1 = layers.Conv2D(8, 3, activation='relu', padding='same')(inputs)
+    conv1 = layers.Conv2D(8, 3, activation='relu', padding='same')(conv1)
+    pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = layers.Conv2D(16, 3, activation='relu', padding='same')(pool1)
+    conv2 = layers.Conv2D(16, 3, activation='relu', padding='same')(conv2)
+    pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = layers.Conv2D(32, 3, activation='relu', padding='same')(pool2)
+    conv3 = layers.Conv2D(32, 3, activation='relu', padding='same')(conv3)
+    pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = layers.Conv2D(64, 3, activation='relu', padding='same')(pool3)
+    conv4 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv4)
+    pool4 = layers.MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    # Bottleneck
+    conv5 = layers.Conv2D(128, 3, activation='relu', padding='same')(pool4)
+    conv5 = layers.Conv2D(128, 3, activation='relu', padding='same')(conv5)
+
+    # Expansive Path
+    up6 = layers.Conv2DTranspose(64, 2, strides=(2, 2), padding='same')(conv5)
+    concat6 = layers.concatenate([up6, conv4], axis=-1)
+    conv6 = layers.Conv2D(64, 3, activation='relu', padding='same')(concat6)
+    conv6 = layers.Conv2D(64, 3, activation='relu', padding='same')(conv6)
+
+    up7 = layers.Conv2DTranspose(32, 2, strides=(2, 2), padding='same')(conv6)
+    concat7 = layers.concatenate([up7, conv3], axis=-1)
+    conv7 = layers.Conv2D(32, 3, activation='relu', padding='same')(concat7)
+    conv7 = layers.Conv2D(32, 3, activation='relu', padding='same')(conv7)
+
+    up8 = layers.Conv2DTranspose(16, 2, strides=(2, 2), padding='same')(conv7)
+    concat8 = layers.concatenate([up8, conv2], axis=-1)
+    conv8 = layers.Conv2D(16, 3, activation='relu', padding='same')(concat8)
+    conv8 = layers.Conv2D(16, 3, activation='relu', padding='same')(conv8)
+
+    up9 = layers.Conv2DTranspose(8, 2, strides=(2, 2), padding='same')(conv8)
+    concat9 = layers.concatenate([up9, conv1], axis=-1)
+    conv9 = layers.Conv2D(8, 3, activation='relu', padding='same')(concat9)
+    conv9 = layers.Conv2D(8, 3, activation='relu', padding='same')(conv9)
+
+    # Output layer
+    outputs = layers.Conv2D(1, 1, activation='sigmoid')(conv9)
+
+    # Create the model
+    model = models.Model(inputs=inputs, outputs=outputs)
+
+    return model
+
+
+def jacard_coef(y_true, y_pred):
+    """
+    Jacard coefficient for model metrics.
+    """
+    y_true = K.cast(y_true, dtype=tf.float32)
+    y_pred = K.cast(y_pred, dtype=tf.float32)
+
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+
+    intersection = K.sum(y_true_f * y_pred_f)
+    union = K.sum(y_true_f) + K.sum(y_pred_f) - intersection
+
+    return (intersection + 1.0) / (union + 1.0)
+
+
+def dice_coef(y_true, y_pred, smooth):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    dice = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return dice
+
+
+def dice_coef_loss(y_true, y_pred, smooth=10e-6):
+    return 1 - dice_coef(y_true, y_pred, smooth)
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("DatasetPath", help="Path to the dataset.")
@@ -23,7 +104,6 @@ args = parser.parse_args()
 if not os.path.isdir(args.DatasetPath):
     print(f'The directory {args.DatasetPath} does not exist')
     exit()
-    
 
 if not os.path.isdir(args.SaveModelPath):
     print(f'The directory {args.SaveModelPath} does not exist')
@@ -31,20 +111,19 @@ if not os.path.isdir(args.SaveModelPath):
 
 path = args.DatasetPath
 
-# Load data from csv file, extract encoded pixels to create masks for training
 masks = pd.read_csv(fr"{path}/train_ship_segmentations_v2.csv")
 masks['ships'] = masks['EncodedPixels'].map(lambda c_row: 1 if isinstance(c_row, str) else 0)
 unique_img_ids = masks.groupby('ImageId').agg({'ships': 'sum'}).reset_index()
 unique_img_ids['is_ship'] = unique_img_ids['ships'].map(lambda x: 1.0 if x > 0 else 0.0)
 
 
-# Function to get mask from encoded pixels
-def rle_decode(mask_rle, IMG_SIZE=(768, 768)):
-    """
+# Function to decode mask
+def rle_decode(mask_rle, IMG_SIZE = (768, 768)):
+    '''
     mask_rle: run-length as string formated (start length)
     shape: (height,width) of array to return
     Returns numpy array, 255 - mask, 0 - background
-    """
+    '''
     s = mask_rle.split()
     starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
     starts -= 1
@@ -63,8 +142,8 @@ try:
 except:
     pass
 
-# Search for all images with number of ships greater than 13
-ids_of_images = unique_img_ids.loc[unique_img_ids.ships > 13, 'ImageId'].tolist()
+# Search for all images with number of ships greater than 0
+ids_of_images = unique_img_ids.loc[unique_img_ids.ships > 0, 'ImageId'].tolist()
 
 # Go through all of those images, decode masks, save all the masks and images to created directories
 for ImageId in ids_of_images:
@@ -82,238 +161,58 @@ for ImageId in ids_of_images:
     cv2.imwrite(path + "/data/masks/" + ImageId, all_masks)
     shutil.copy(path + "/train_v2/" + ImageId, path + "/data/images")
 
-# Use minmaxscaler instead of just dividing by 255 while patching
-scaler = MinMaxScaler()
 
-# Define root directory from which all the images and masks will be taken
-root_directory = path + '/data/'
+# Create the U-Net model
+model = unet_model_simplified()
 
-# Define size of patched images
-# 256 stands for 256x256 pixels
-patch_size = 256
-
-# Read images from repsective 'images' subdirectory
-# And then divide all images into patches of 256x256x3.
-image_dataset = []
-for path, subdirs, files in os.walk(root_directory):
-    # print(path)
-    dirname = path.split(os.path.sep)[-1]
-    if dirname == 'images':
-        images_list = os.listdir(path)
-        for i, image_name in enumerate(images_list):
-            image = cv2.imread(path + "/" + image_name, 1)
-            SIZE_X = 768  # Width of input images
-            SIZE_Y = 768  # Height of input images
-            image = Image.fromarray(image)
-            image = image.crop((0, 0, SIZE_X, SIZE_Y))  # Crop from top left corner
-            image = np.array(image)
-
-            # Extract patches from each image
-            print("Now patchifying image:", path + "/" + image_name)
-            # Step = patch_size for no overlaping
-            patches_img = patchify(image, (patch_size, patch_size, 3), step=patch_size)
-
-            for i in range(patches_img.shape[0]):
-                for j in range(patches_img.shape[1]):
-                    single_patch_img = patches_img[i, j, :, :]
-
-                    # Use minmaxscaler instead of just dividing by 255.
-                    single_patch_img = scaler.fit_transform(single_patch_img.reshape(-1, single_patch_img.shape[-1])).reshape(single_patch_img.shape)
-
-                    single_patch_img = single_patch_img[0]  # Drop the extra unecessary dimension that patchify adds.
-                    image_dataset.append(single_patch_img)
+# Display the model summary
+model.summary()
 
 
-# Do the same for masks
-mask_dataset = []
-for path, subdirs, files in os.walk(root_directory):
-    # print(path)
-    dirname = path.split(os.path.sep)[-1]
-    if dirname == 'masks':
-        masks_list = os.listdir(path)
-        for i, mask_name in enumerate(masks_list):
-            mask = cv2.imread(path + "/" + mask_name, 1)
-            mask = cv2.cvtColor(mask, cv2.COLOR_BGR2RGB)
-            SIZE_X = 768  # Width of input images
-            SIZE_Y = 768  # Height of input images
-            mask = Image.fromarray(mask)
-            mask = mask.crop((0, 0, SIZE_X, SIZE_Y))  # Crop from top left corner
-            mask = np.array(mask)
+# Get images and masks paths in dataframe
+train_data_dir = path + '/data'
+train_images_dir = train_data_dir + '/images'
+train_masks_dir = train_data_dir + '/masks'
 
-            # Extract patches from each image
-            print("Now patchifying mask:", path + "/" + mask_name)
-            patches_mask = patchify(mask, (patch_size, patch_size, 3), step=patch_size)
+file_list = os.listdir(train_images_dir)
+df = pd.DataFrame({'image_path': [os.path.join(train_images_dir, img) for img in file_list],
+                   'mask_path': [os.path.join(train_masks_dir, img) for img in file_list]})
 
-            for i in range(patches_mask.shape[0]):
-                for j in range(patches_mask.shape[1]):
-                    single_patch_mask = patches_mask[i, j, :, :]
-                    single_patch_mask = single_patch_mask[0]  # Drop the extra unecessary dimension that patchify adds.
-                    mask_dataset.append(single_patch_mask)
+image_size = (768, 768)
 
+# Create an ImageDataGenerator for data augmentation, normalization and validation_split
+data_gen_args = dict(rescale=1./255, validation_split=0.2)
+image_datagen = ImageDataGenerator(**data_gen_args)
+mask_datagen = ImageDataGenerator(**data_gen_args)
 
-# Convert images and masks into numpy array
-image_dataset = np.array(image_dataset)
-mask_dataset = np.array(mask_dataset)
+# Create data generators for training images and masks
+seed = 1
+image_generator = image_datagen.flow_from_dataframe(
+    df,
+    x_col='image_path',
+    target_size=image_size,
+    class_mode=None,
+    seed=seed,
+    batch_size=32
+)
 
-# Define colors of objects in masks
-Ship = np.array((255, 255, 255))
-NotShip = np.array((0, 0, 0))
+mask_generator = image_datagen.flow_from_dataframe(
+    df,
+    x_col='mask_path',
+    target_size=image_size,
+    color_mode='grayscale',
+    class_mode=None,
+    seed=seed,
+    batch_size=32
+)
 
-# Create numpy array for labels with the same shape as masks
-label = single_patch_mask
-
-
-# Now replace RGB to integer values to be used as labels.
-# Find pixels with combination of RGB for the above defined arrays...
-# if matches then replace all values in that pixel with a specific integer
-def rgb_to_2D_label(label):
-    """
-    Suply our label masks as input in RGB format.
-    Replace pixels with specific RGB values ...
-    """
-    label_seg = np.zeros(label.shape, dtype=np.uint8)
-    label_seg[np.all(label == Ship, axis=-1)] = 0
-    label_seg[np.all(label == NotShip, axis=-1)] = 1
-
-    label_seg = label_seg[:, :, 0]  # Just take the first channel, no need for all 3 channels
-
-    return label_seg
+train_generator = zip(image_generator, mask_generator)
 
 
-labels = []
-for i in range(mask_dataset.shape[0]):
-    label = rgb_to_2D_label(mask_dataset[i])
-    labels.append(label)
+model.compile(optimizer='adam',
+              loss=dice_coef_loss,
+              metrics=[jacard_coef, Precision(), Recall()])
 
-labels = np.array(labels)
-labels = np.expand_dims(labels, axis=3)
+history = model.fit(train_generator, epochs=2, steps_per_epoch=len(image_generator))
 
-
-def multi_unet_model(n_classes=4, IMG_HEIGHT=256, IMG_WIDTH=256, IMG_CHANNELS=1):
-    """
-    UNet model for problems with 2 and more classes.
-
-    :param n_classes: Number of classes to classify.
-    :param IMG_HEIGHT: Height of the input image.
-    :param IMG_WIDTH: Width of the input image.
-    :param IMG_CHANNELS: Number of channels of the image.
-    :return: tf.keras.Model
-    """
-    inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
-    s = inputs
-
-    # Contraction path
-    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(s)
-    c1 = Dropout(0.1)(c1)
-    c1 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c1)
-    p1 = MaxPooling2D((2, 2))(c1)
-
-    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p1)
-    c2 = Dropout(0.1)(c2)
-    c2 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c2)
-    p2 = MaxPooling2D((2, 2))(c2)
-
-    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p2)
-    c3 = Dropout(0.2)(c3)
-    c3 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c3)
-    p3 = MaxPooling2D((2, 2))(c3)
-
-    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p3)
-    c4 = Dropout(0.2)(c4)
-    c4 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c4)
-    p4 = MaxPooling2D(pool_size=(2, 2))(c4)
-
-    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(p4)
-    c5 = Dropout(0.3)(c5)
-    c5 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c5)
-
-    # Expansive path
-    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(c5)
-    u6 = concatenate([u6, c4])
-    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u6)
-    c6 = Dropout(0.2)(c6)
-    c6 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c6)
-
-    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(c6)
-    u7 = concatenate([u7, c3])
-    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u7)
-    c7 = Dropout(0.2)(c7)
-    c7 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c7)
-
-    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(c7)
-    u8 = concatenate([u8, c2])
-    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u8)
-    c8 = Dropout(0.1)(c8)
-    c8 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c8)
-
-    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same')(c8)
-    u9 = concatenate([u9, c1], axis=3)
-    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(u9)
-    c9 = Dropout(0.1)(c9)
-    c9 = Conv2D(16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(c9)
-
-    outputs = Conv2D(n_classes, (1, 1), activation='softmax')(c9)
-
-    model = Model(inputs=[inputs], outputs=[outputs])
-
-    return model
-
-
-def jacard_coef(y_true, y_pred):
-    """
-    Jacard coefficient for model metrics.
-    """
-    y_true_f = K.flatten(y_true)
-    y_pred_f = K.flatten(y_pred)
-    intersection = K.sum(y_true_f * y_pred_f)
-    return (intersection + 1.0) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + 1.0)
-
-
-n_classes = len(np.unique(labels))
-labels_cat = to_categorical(labels, num_classes=n_classes)
-X_train, X_test, y_train, y_test = train_test_split(image_dataset, labels_cat, test_size=0.20, random_state=42)
-
-# Parameters for model
-weights = [0.5, 0.5]
-dice_loss = sm.losses.DiceLoss(class_weights=weights)
-focal_loss = sm.losses.CategoricalFocalLoss()
-total_loss = dice_loss + (1 * focal_loss)
-
-
-IMG_HEIGHT = X_train.shape[1]
-IMG_WIDTH = X_train.shape[2]
-IMG_CHANNELS = X_train.shape[3]
-
-metrics = ['accuracy', jacard_coef]
-
-
-def get_model():
-    return multi_unet_model(n_classes=n_classes, IMG_HEIGHT=IMG_HEIGHT, IMG_WIDTH=IMG_WIDTH, IMG_CHANNELS=IMG_CHANNELS)
-
-
-model = get_model()
-model.compile(optimizer='adam', loss=total_loss, metrics=metrics)
-# model.compile(optimizer='adam', loss=dice_loss, metrics=metrics)
-# model.summary()
-
-# Train the model
-history1 = model.fit(X_train, y_train,
-                     batch_size=32,
-                     verbose=1,
-                     epochs=20,
-                     validation_data=(X_test, y_test),
-                     shuffle=True)
-
-# Save the model
 model.save(args.SaveModelPath + "/model_4.h5")
-
-# IOU
-y_pred = model.predict(X_test)
-y_pred_argmax = np.argmax(y_pred, axis=3)
-y_test_argmax = np.argmax(y_test, axis=3)
-
-# Using built in keras function for IoU
-# n_classes = 2
-IOU_keras = MeanIoU(num_classes=n_classes)
-IOU_keras.update_state(y_test_argmax, y_pred_argmax)
-print("Mean IoU =", IOU_keras.result().numpy())
